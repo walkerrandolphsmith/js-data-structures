40d0157ce6056df4137be4fc351fdb8311f93442
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-10 14:25:30 -0400

    BinarySearchTrees can add elements.

--- src/BinarySearchTree/index.js
+++ src/BinarySearchTree/index.js
@@ -2,4 +2,26 @@ import BinaryTree, { Node } from './../BinaryTree';
 
 export default class BinarySearchTree extends BinaryTree {
 
+    add = (element, comparator) => {
+        const { wasAdded, root } = this.addNode(this.root, element, comparator);
+        this.root = root;
+        return wasAdded;
+    };
+
+    addNode = (currentRoot, element, comparator = (e, d) => e > d) => {
+        if(currentRoot.data) {
+            const comparison = comparator(element, currentRoot.data);
+            if(comparison === 0) {
+                return { wasAdded: false, root: currentRoot };
+            } else if(comparison < 0) {
+                currentRoot.left = this.addNode(currentRoot.left, element, comparator);
+                return { wasAdded: false, root: currentRoot };
+            } else {
+                currentRoot.right = this.addNode(currentRoot.right, element, comparator);
+                return { wasAdded: false, root: currentRoot };
+            }
+        } else {
+            return { wasAdded: true, root: new Node(element) };
+        }
+    }
 }
--- src/BinarySearchTree/index.spec.js
+++ src/BinarySearchTree/index.spec.js
@@ -2,8 +2,9 @@ import expect from 'expect';
 import BinaryTree, { Node } from './../BinaryTree';
 import BinarySearchTree from './index';
 
-describe.only('src/BinarySearchTree', () => {
+describe('src/BinarySearchTree', () => {
     let tree;
+    let element;
     describe('Given no parameters', () => {
         describe('When creating a BinarySearchTree', () => {
             beforeEach(() => {
@@ -12,6 +13,25 @@ describe.only('src/BinarySearchTree', () => {
 
             it('should be a BinaryTree', () => {
                 expect(tree instanceof BinaryTree).toBeTruthy();
+            });
+        })
+    });
+
+    describe('Given an element and empty Binary Search Tree', () => {
+        describe('When adding the element', () => {
+            let wasAdded;
+            beforeEach(() => {
+                tree = new BinarySearchTree();
+                element = { id: 1 };
+                wasAdded = tree.add(element);
+            });
+
+            it('should indicate that an element was added', () => {
+                expect(wasAdded).toBeTruthy();
+            });
+            
+            it('should have a root node that contains the element', () => {
+                expect(tree.getData()).toEqual(element);
             });
         })
     });
