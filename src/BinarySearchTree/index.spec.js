6a7eac867c590343109b78061961807aebabba5a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-10 18:01:29 -0400

    When using the compareOnObjectProperty comparator, when the elements' prop X values are equal it returns 1 when the first element's prop X value is less than the second it return -1 when the first element's prop X value is greater than the second it returns 1

--- src/BinarySearchTree/index.spec.js
+++ src/BinarySearchTree/index.spec.js
@@ -21,6 +21,30 @@ describe('src/BinarySearchTree', () => {
         });
     });
 
+    describe('Given the compareOnObjectProperty comparator and object property name id', () => {
+        let comparator;
+        beforeEach(() => {
+            comparator = compareOnObjectProperty('id');
+        });
+        describe('When comparing the elements that are equal', () => {
+            it('should return zero', () => {
+                expect(comparator({ id: 1 }, { id: 1 })).toEqual(0);
+            });
+        });
+
+        describe('When comparing two elements such that the first is less than the second', () => {
+            it('should return 1', () => {
+                expect(comparator({ id: 1 }, { id: 2 })).toEqual(-1);
+            });
+        });
+
+        describe('When comparing two elements such that the first is greater than the second', () => {
+            it('should return 1', () => {
+                expect(comparator({ id: 5 }, { id: 2 })).toEqual(1);
+            });
+        });
+    });
+
 
     describe('Given no parameters', () => {
         describe('When creating a BinarySearchTree', () => {
