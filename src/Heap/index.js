ec3d2539a4f7c789d2205ffdf2f5e9513421b830
walker smith <walker.smith@versionone.com>
2016-07-11 13:24:35 -0400

    When inserting elements of strictly decreasing values into an empty heap the largest values are bubbled to the top.

--- src/Heap/index.js
+++ src/Heap/index.js
@@ -1,3 +1,5 @@
+import { defaultComparator } from './../BinaryTree';
+
 export default class Heap {
     constructor(capacity) {
         this.capacity = typeof capacity === 'number' ? capacity : 0;
@@ -7,13 +9,28 @@ export default class Heap {
 
     isEmpty = () => this.heap.length <= 0;
 
-    insert = (element) => {
+    insert = (element, comparator = defaultComparator) => {
         if(this.nextEmpty >= this.capacity) {
             return false;
         } else {
             this.heap[this.nextEmpty] = element;
-            this.nextEmpty++;
+            let child = this.nextEmpty++;
+            if(child > 0) {
+                let parent = (child - 1) / 2;
+                const comparison = comparator(this.heap[parent], this.heap[child]);
+                while(parent >= 0 && comparison === 1) {
+                    this.swap(parent, child);
+                    child = parent;
+                    parent = (child - 1) / 2;
+                }
+            }
             return true;
         }
     };
+
+    swap = (parent, child) => {
+        const temp = this.heap[parent];
+        this.heap[parent] = this.heap[child];
+        this.heap[child] = temp;
+    }
 }
--- src/Heap/index.spec.js
+++ src/Heap/index.spec.js
@@ -1,5 +1,6 @@
 import expect from 'expect';
 import Heap from './index';
+import { compareOnObjectProperty } from './../BinaryTree';
 
 describe('src/BinarySearchTree', () => {
     let heap;
@@ -7,6 +8,7 @@ describe('src/BinarySearchTree', () => {
     let element;
     let actual;
     let largerElement;
+    let comparator = compareOnObjectProperty('id');
     describe('Given no parameters', () => {
         describe('When creating a Heap', () => {
             beforeEach(() => {
@@ -59,11 +61,35 @@ describe('src/BinarySearchTree', () => {
             });
         });
 
-        describe('When inserting an element of strictly increasing values', () => {
+        describe('When inserting elements of strictly increasing values', () => {
             beforeEach(() => {
                 largerElement = { id: 2 };
-                heap.insert(element);
-                actual = heap.insert(largerElement);
+                heap.insert(element, comparator);
+                actual = heap.insert(largerElement, comparator);
+            });
+
+            it('should not be empty', () => {
+                expect(heap.isEmpty()).toBeFalsy();
+            });
+
+            it('should return true indicating the element was inserted', () => {
+                expect(actual).toBeTruthy();
+            });
+
+            it('should have element at the first index', () => {
+                expect(heap.heap[0]).toEqual(element);
+            });
+
+            it('should have the larger element at the second index', () => {
+                expect(heap.heap[1]).toEqual(largerElement);
+            });
+        });
+
+        describe('When inserting elements of strictly decreasing values', () => {
+            beforeEach(() => {
+                largerElement = { id: 2 };
+                heap.insert(largerElement, comparator);
+                actual = heap.insert(element, comparator);
             });
 
             it('should not be empty', () => {
