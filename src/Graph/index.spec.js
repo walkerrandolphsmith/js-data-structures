1e53ad97142f778ee6de9f4dec7e35725f3b74db
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-14 22:23:33 -0400

    Adding a vertex to an empty graph return true indicating it was added.

--- src/Graph/index.spec.js
+++ src/Graph/index.spec.js
@@ -3,6 +3,7 @@ import Graph from './index';
 
 describe('src/Graph', () => {
     let graph;
+    let vertex;
     describe('Given no parameters', () => {
         describe('When creating a graph', () => {
             beforeEach(() => {
@@ -11,6 +12,23 @@ describe('src/Graph', () => {
 
             it('should create a graph', () => {
                 expect(graph instanceof Graph).toBeTruthy();
+            });
+        });
+    });
+
+    describe('Given an empty graph', () => {
+        beforeEach(() => {
+            graph = new Graph();
+        });
+
+        describe('When adding a vertex', () => {
+            let actual;
+            beforeEach(() => {
+                vertex = { id: 1 };
+                actual = graph.addVertex(vertex);
+            });
+            it('should return true indicating the vertex was added', () => {
+                expect(actual).toBeTruthy();
             });
         });
     });
