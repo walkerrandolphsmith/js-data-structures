f809e425dba2f3a928994b835aa81de3b5ed1983
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-18 00:21:55 -0400

    Don't use a beforeEach within a when statement unless there are multiple assertions

--- src/Graph/index.spec.js
+++ src/Graph/index.spec.js
@@ -4,18 +4,14 @@ import { objectPropComparator } from './../Comparators/EqualityComparators';
 
 describe('src/Graph', () => {
     let graph;
-    let vertex = { id: 1 };
+    let v = { id: 1 };
+    let w = { id: 2 };
     let comparator = objectPropComparator('id');
-    let actual;
 
     describe('Given no parameters', () => {
         describe('When creating a graph', () => {
-            beforeEach(() => {
-                graph = new Graph(comparator);
-            });
-
             it('should create a graph', () => {
-                expect(graph instanceof Graph).toBeTruthy();
+                expect(new Graph(comparator) instanceof Graph).toBeTruthy();
             });
         });
     });
@@ -26,41 +22,26 @@ describe('src/Graph', () => {
         });
 
         describe('When adding a vertex', () => {
-            beforeEach(() => {
-                actual = graph.addVertex(vertex);
-            });
             it('should return true indicating the vertex was added', () => {
-                expect(actual).toBeTruthy();
+                expect(graph.addVertex(v)).toBeTruthy();
             });
         });
 
         describe('When finding a vertex not in the graph', () => {
-            beforeEach(() => {
-                actual = graph.findVertex(vertex);
-            });
-
             it('should return the vertex', () => {
-                expect(actual).toBeFalsy();
+                expect(graph.findVertex(v)).toBeFalsy();
             });
         });
 
         describe('when getting the degree of a vertex not in the graph', () => {
-            beforeEach(() => {
-                actual = graph.degree(vertex);
-            });
-
             it('it should return -1', () => {
-                expect(actual).toEqual(-1);
+                expect(graph.degree(v)).toEqual(-1);
             });
         });
 
         describe('when adding an edge between one or more vertex not in the graph', () => {
-            beforeEach(() => {
-                actual = graph.addEdge(vertex, vertex);
-            });
-
             it('it should return false indicating the edge was not created', () => {
-                expect(actual).toEqual(false);
+                expect(graph.addEdge(v, v)).toEqual(false);
             });
         });
     });
@@ -68,93 +49,83 @@ describe('src/Graph', () => {
     describe('Given a non empty graph', () => {
         beforeEach(() => {
             graph = new Graph(comparator);
-            graph.addVertex(vertex);
+            graph.addVertex(v);
         });
 
         describe('When adding a vertex already in graph', () => {
-            beforeEach(() => {
-                actual = graph.addVertex(vertex);
-            });
-
             it('should return false indicating the vertex was not added', () => {
-                expect(actual).toBeFalsy();
+                expect(graph.addVertex(v)).toBeFalsy();
             });
         });
 
         describe('when getting the degree of a vertex in the graph that does not make an edge', () => {
-            beforeEach(() => {
-                actual = graph.degree(vertex);
-            });
-
             it('it should return 0', () => {
-                expect(actual).toEqual(0);
+                expect(graph.degree(v)).toEqual(0);
             });
         });
 
         describe('When getting all vertices', () => {
-            beforeEach(() => {
-                actual = graph.getVertices();
-            });
             it('should return a list of all vertices in graph', () => {
-                expect(actual).toInclude(vertex);
+                expect(graph.getVertices()).toInclude(v);
             });
         });
 
         describe('when finding a vertex in the graph', () => {
-            beforeEach(() => {
-                actual = graph.findVertex(vertex);
-            });
-
             it('it should return the vertex', () => {
-                expect(actual).toEqual(vertex);
+                expect(graph.findVertex(v)).toEqual(v);
             });
         });
 
         describe('when adding an edge between two vertices in the graph', () => {
-            beforeEach(() => {
-                actual = graph.addEdge(vertex, vertex);
-            });
-
             it('it should return true indicating the edge was added', () => {
-                expect(actual).toEqual(true);
+                expect(graph.addEdge(v, v)).toEqual(true);
             });
         });
     });
 
-    describe('given a graph with edge', () => {
+    describe('given a graph with edge between a vertex and itself', () => {
         beforeEach(() => {
             graph = new Graph(comparator);
-            graph.addVertex(vertex);
-            graph.addEdge(vertex, vertex)
+            graph.addVertex(v);
+            graph.addEdge(v, v)
         });
 
         describe('when getting the degree of a vertex in the graph that makes an edge', () => {
-            beforeEach(() => {
-                actual = graph.degree(vertex);
-            });
-
-            it('it should return -1', () => {
-                expect(actual).toEqual(1);
+            it('it should return 1', () => {
+                expect(graph.degree(v)).toEqual(1);
             });
         });
 
         describe('when getting all the edges associated with a vertex', () => {
-            beforeEach(() => {
-                actual = graph.getEdges(vertex);
-            });
-
             it('it should return all the vertices that are incedent.', () => {
-                expect(actual).toInclude(vertex);
+                expect(graph.getEdges(v)).toInclude(v);
             });
         });
 
         describe('when getting all the edges associated with a vertex not in the graph', () => {
-            beforeEach(() => {
-                actual = graph.getEdges({ id: 5 });
-            });
-
             it('it should return all the vertices that are incedent.', () => {
-                expect(actual).toBeFalsy();
+                expect(graph.getEdges({ id: 5 })).toBeFalsy();
+            });
+        });
+    });
+
+    describe('given a graph with edge between two distinct vertices', () => {
+        beforeEach(() => {
+            graph = new Graph(comparator);
+            graph.addVertex(v);
+            graph.addVertex(w);
+            graph.addEdge(v, w)
+        });
+
+        describe('when getting the degree of a vertex in the graph that makes an edge', () => {
+            it('it should return 1', () => {
+                expect(graph.degree(v)).toEqual(1);
+            });
+        });
+
+        describe('when getting all the edges associated with a vertex', () => {
+            it('it should return all the vertices that are incedent.', () => {
+                expect(graph.getEdges(v)).toInclude(w);
             });
         });
     });
