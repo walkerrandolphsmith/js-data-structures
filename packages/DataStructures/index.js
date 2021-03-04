196dbf2fa67e2b7e7f4a14606702f96b67fdbaff
walker smith <wsmith@collab.net>
2021-03-04 09:01:43 -0500

    remove data-strucutres

--- packages/DataStructures/index.js
+++
@@ -1,8 +0,0 @@
-export { default as LinkedList } from '@walkerrandolphsmith/linked-list';
-export { default as Stack } from '@walkerrandolphsmith/stack';
-export { default as BinaryTree } from '@walkerrandolphsmith/binary-tree';
-export { default as BinarySearchTree } from '@walkerrandolphsmith/binary-search-tree';
-export { default as HuffmanTree } from '@walkerrandolphsmith/huffman-tree';
-export { default as AdjacencyMatrix } from '@walkerrandolphsmith/adjacency-matrix';
-export { default as Graph } from '@walkerrandolphsmith/graph';
-export * from '@walkerrandolphsmith/comparators';
